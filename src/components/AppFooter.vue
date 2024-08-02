b489a5fb8541f9e973955bda96b12a4854f8a84d
EnricoClem <enrico.clem94@gmail.com>
2024-08-02 12:31:24 +0200

    ADD - footer component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,10 +5,113 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Footer</h1>
-    </div>
+    <footer>
+        <div class="footer-container">
+            <div class="row align">
+                <!-- TITLE -->
+                <div class="col-3">
+                    <h3>Grand Hotel Esplanada</h3>
+                    <p class="caption">© Copyright  2021 Grand Hotel Esplanada. All rights reserved.</p>
+                </div>
+                <!-- CONTACT INFO -->
+                <div class="col-6 row-col gap-sm">
+                    <p class="caption text-center">Via delle Dune 6 - 30020 Bibione Pineda VE Italy</p>
+                    <p class="text-center gap-md row justify-center">
+                        <a href="mailto:info@esplanada.it">info@esplanada.it</a>
+                        <a href="mailto:info@hotelesplanada.it">info@hotelesplanada.it</a>
+                    </p>
+                    <p class="caption text-center gap-md row justify-center">
+                        <a href="tel:+39 043143260">+39 043143260</a>
+                        <a href="tel:+39 0431430831">+39 0431430831</a> 
+                        <a href="tel:+39 0432430832">+39 0432430832 (fax)</a> 
+                    </p>
+                </div>
+                <!-- SOCIAL BUTTONS -->
+                <div class="col-3 row justify-end gap-md align">
+                    <a href="https://wa.me/+393514454589" class="wtsp-btn row gap-sm hover-container-wtsp">
+                        <p class="caption">Scrivici direttamente <br> su WhatsApp</p>
+                        <img class="social-foot" src="../assets/WTSP-logo-1.svg" />
+                        <img class="social-foot-hover" src="../assets/WTSP-logo-2.svg" />
+                    </a>
+                    <a href="https://www.facebook.com/grandhotelesplanada" class="hover-container">
+                        <img class="social-foot" src="../assets/FB-logo-1.svg" />
+                        <img class="social-foot-hover" src="../assets/FB-logo-2.svg" />
+                    </a>
+                    <a href="https://www.instagram.com/grandhotelesplanada/?utm_medium=copy_link" class="hover-container">
+                        <img class="social-foot" src="../assets/INST-logo-1.svg" />
+                        <img class="social-foot-hover" src="../assets/INST-logo-2.svg" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+@use '../style/partials/variables';
+
+.footer-container {
+    background-color: variables.$color-b;
+    width: 100%;
+    padding: 20px 40px;
+    color: variables.$color-g;
+}
+
+.social-foot {
+    filter: brightness(0) saturate(100%) invert(84%) sepia(21%) saturate(1%) hue-rotate(314deg) brightness(97%) contrast(85%);
+    width: 30px;
+    position: absolute;
+}
+
+.social-foot-hover {
+    display: none;
+    position: absolute;
+    width: 30px;
+    filter: brightness(0) saturate(100%) invert(84%) sepia(21%) saturate(1%) hue-rotate(314deg) brightness(97%) contrast(85%);
+}
+
+.wtsp-btn {
+    padding: 8px 50px 8px 8px;
+    border: solid 1px variables.$color-g;
+}
+
+.wtsp-btn .social-foot {
+    width: 25px;
+    left: 120px;
+    top: 12.5px;
+}
+
+.wtsp-btn .social-foot-hover {
+    width: 25px;
+    left: 120px;
+    top: 12.5px;
+}
+
+.hover-container {
+    position: relative;
+    width: 30px;
+    height: 30px;
+}
+
+.hover-container:hover .social-foot-hover {
+    display: block;
+}
+
+.hover-container:hover .social-foot {
+    display: none;
+}
+
+.hover-container-wtsp {
+    position: relative;
+}
+
+.hover-container-wtsp:hover .social-foot-hover {
+    display: block;
+}
+
+.hover-container-wtsp:hover .social-foot {
+    display: none;
+}
+
 </style>
